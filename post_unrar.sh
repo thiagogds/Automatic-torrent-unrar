3364529f0c03066a97b2ff22c313fd8114a55255
Thiago Garcia <thiagogds14@gmail.com>
2012-04-16 11:34:56 -0300

    Extrai para a mesma pasta do download

--- post_unrar.sh
+++ post_unrar.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-mkdir ~/Movies/$TR_TORRENT_NAME
 cd $TR_TORRENT_DIR/$TR_TORRENT_NAME
 CURRENT_FILE=$(ls | grep rar | xargs echo)
-/usr/local/Cellar/unrar/4.0.7/bin/unrar x $CURRENT_FILE ~/Movies/$TR_TORRENT_NAME
+/usr/local/Cellar/unrar/4.0.7/bin/unrar x $CURRENT_FILE $TR_TORRENT_DIR/$TR_TORRENT_NAME
